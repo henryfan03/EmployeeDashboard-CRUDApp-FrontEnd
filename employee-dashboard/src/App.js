a5949a8a5f87a582a3acfd91d495ef0801b373a9
Henry Fan <henryfan@macbook-air.mynetworksettings.com>
2023-05-22 00:15:11 -0400

    fixed an issue preventing employees from being displayed

--- employee-dashboard/src/App.js
+++ employee-dashboard/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
                 <Route exact path="/employees/:id" component={EmployeeContainer} />
                 <Route exact path="/tasks" component={AllTasksContainer} />
                 <Route exact path="/newtask" component={NewTaskContainer} />
-                <Route exact path="/tasks/:id" component={TaskContainer} />
+                <Route exact path="/task/:id" component={TaskContainer} />
                 <Route exact path="/edittask/:id" component={EditTaskContainer} />
                 {/*<Route exact path="/newemployee" component={NewEmployeeContainer} />*/}
                 {/*<Route exact path="/editemployee/:id" component={EditEmployeeContainer} />*/}
